815d4cf7a782b77696c7743b960fa4117a9f06d0
CzNorbi <norbertczier@gmail.com>
2022-05-16 13:48:46 +0200

    BookContext created

---
+++ WebApi_Server/Repositories/BookContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi_Common.Models;
+
+namespace WebApi_Server.Repositories
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source=(localdb)\\mssqllocaldb;Database=ServerDb;Integrated Security=True;");
+        }
+    }
+}
