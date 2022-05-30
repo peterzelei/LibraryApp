70a6495e5fb060a46c3ec4cb87f439ef67886cca
Peter Zelei <zeleipeter21@gmail.com>
2022-05-30 18:46:06 +0200

    Added LibraryCard page (search borrowed books by name)

---
+++ LibraryClient/Pages/LibraryCard.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using WebApi_Common.Models;
+
+namespace LibraryClient.Pages
+{
+    public partial class LibraryCard
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        public string Name { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Books = (await HttpClient.GetFromJsonAsync<Book[]>("book")).Where(b => b.NameOfBorrower == Name && b.IsBorrowed)
+                                                                       .OrderBy(b => b.DateOfReturn);
+            await base.OnInitializedAsync();
+        }
+    }
+}
