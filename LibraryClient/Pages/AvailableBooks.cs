3956a0b1bfe374092060c2b1f10c6d738668c0ac
Peter Zelei <zeleipeter21@gmail.com>
2022-05-30 14:46:12 +0200

    LibraryClient and AvailableBooks page added

---
+++ LibraryClient/Pages/AvailableBooks.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using WebApi_Common.Models;
+
+namespace LibraryClient.Pages
+{
+    public partial class AvailableBooks
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Books = (await HttpClient.GetFromJsonAsync<Book[]>("book")).Where(b => b.IsBorrowed == false);
+            await base.OnInitializedAsync();
+        }
+    }
+}
