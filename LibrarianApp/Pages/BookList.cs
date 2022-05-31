41d1c06a7ab663d50ccc2e7cf291892a43ee2d8c
Peter Zelei <zeleipeter21@gmail.com>
2022-05-31 12:08:10 +0200

    Added searchbox to BookList page

--- LibrarianApp/Pages/BookList.cs
+++ LibrarianApp/Pages/BookList.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using System.Net.Http.Json;
 using WebApi_Common.Models;
 using Microsoft.AspNetCore.Components.Web;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LibrarianApp.Pages
 {
@@ -14,13 +16,18 @@ namespace LibrarianApp.Pages
 
         public Book[] Books { get; set; }
 
+        public string SearchText = "";
+
+        IEnumerable<Book> FilteredBooks => 
+            Books.Where(b => b.Title.ToLower().Contains(SearchText.ToLower()) || b.Author.ToLower().Contains(SearchText.ToLower()));
+
         protected override async Task OnInitializedAsync()
         {
             Books = await HttpClient.GetFromJsonAsync<Book[]>("book");
             await base.OnInitializedAsync();
         }
 
-        private async Task  UpdateRentalStatus(long BookID,Book Book)
+        private async Task UpdateRentalStatus(long BookID, Book Book)
         {
             Book.Initialize();
             await HttpClient.PutAsJsonAsync($"book/{BookID}", Book);
